931e36aaab19d9fb15ddf74af6da3ea96d5e1e02
Mcvoltrudge Rejouis <37338175+MRejouis2016@users.noreply.github.com>
2018-04-04 23:08:51 -0400

    Add files via upload

--- window.cpp
+++ window.cpp
@@ -1,1 +1,15 @@
-# window.cpp
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+   int strSize;
+   string homescreen;
+   string menu;
+   string program;
+   string start;
+   
+   cout << " Microsoft Windows displays the " << "homescreen, " <<"the menu, " << "the program, " << "and " << " the start " << "button";
+   
+   return 0;
+}
